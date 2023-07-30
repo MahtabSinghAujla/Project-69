1b6f368238470a996f0d36ffcde97107a608c4b9
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2023-07-30 12:30:21 +1000

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -15,7 +15,7 @@ export default class TransactionScreen extends Component {
   }
 
   getCameraPermissions = async domState => {
-    //const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
     //const { status } = Permissions.askAsync(Permissions.CAMERA);
     //const { status } = await Permissions.askAsync(Permissions);
     //const { status } = await Permissions.askAsync(CAMERA);
@@ -55,37 +55,16 @@ export default class TransactionScreen extends Component {
           {hasCameraPermissions ? scannedData : "Request for Camera Permission"}
         </Text>
         
-        {/*
-          <TouchableOpacity
-            style={[styles.button, { marginTop: 25 }]}
-            onPress= this.getCameraPermissions("scanner")
-          >
-            <Text style={styles.buttonText}>Scan QR Code</Text>
-          </TouchableOpacity> 
-        */}
-        {/*
-          <TouchableOpacity
-            style={[styles.button, { marginTop: 25 }]}
-            onPress={() => this.getCameraPermissions("scanner")}
-          >
-            <Text style={styles.buttonText}>Scan QR Code</Text>
-          </TouchableOpacity> 
-        */}
-        {/*
-          <TouchableOpacity
-            style={[styles.button, { marginTop: 25 }]}
-            onPress={() => this.getCameraPermissions()}
-          >
-            <Text style={styles.buttonText}>Scan QR Code</Text>
-          </TouchableOpacity> 
-        */}
 
-        {/*
+        
           <TouchableOpacity
             style={[styles.button, { marginTop: 25 }]}
             onPress={() => this.getCameraPermissions("scanner")}
-          />
-        */}
+          >
+            <Text style={styles.buttonText}>Scan QR Code</Text>
+          </TouchableOpacity> 
+        
+
        
       </View>
     );
